bdc7ed723f160ae9e4fc95af52af818cc867f82a
Victor Cronemberger <victorccronemberger@users.noreply.github.com>
2026-08-07 16:58:13 -0300

    fix: secure production session defaults

--- config/session.php
+++ config/session.php
@@ -31,7 +31,7 @@ return [
     |
     */
 
-    'lifetime' => 43200,
+    'lifetime' => env('SESSION_LIFETIME', 43200),
 
     'expire_on_close' => false,
 
@@ -168,7 +168,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', app()->environment('production')),
 
     /*
     |--------------------------------------------------------------------------
@@ -196,6 +196,6 @@ return [
     |
     */
 
-    'same_site' => null,
+    'same_site' => env('SESSION_SAME_SITE', 'lax'),
 
 ];
